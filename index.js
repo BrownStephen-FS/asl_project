434c4dd16fb9b107243148f1a6bddb0ab8675cab
Stephen Brown <sabrown2@student.fullsail.edu>
2022-08-31 20:51:01 -0400

    Fixed and issue found during testing

--- index.js
+++ index.js
@@ -10,16 +10,16 @@ app.get("/products/all", (req, res) => {
   res.send(`GET Products: ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`);
 });
 
+//GET /products/8719-small-red HTTP/1.1
+//Expected curl response: GET Products: 8719, small, red
+app.get("/products/:num-:size-:color", (req, res) => {
+ res.send(`GET Products: ${req.params.num}, ${req.params.size}, ${req.params.color}`);
+});
+
 //GET /products/8719 HTTP/1.1
 //Expected curl response: GET Products: 8719
 app.get("/products/:num", (req, res) => {
   res.send(`GET Products: ${req.params.num}`);
-});
-
-//GET /products/8719 HTTP/1.1
-//Expected curl response: GET Products: 8719, small, red
-app.get("/products/:num-:size-:color", (req, res) => {
-  res.send(`GET Products: ${req.params.num}, ${req.params.size}, ${req.params.color}`);
 });
 
 app.listen(3000);
