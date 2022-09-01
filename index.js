5c2b4c0a4eeb9d524c8a8da3d5cbe263da192d9b
Stephen Brown <sabrown2@student.fullsail.edu>
2022-08-31 20:19:04 -0400

    Committing completed 1.2-advanced-routing

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const app = express();
+
+//GET /products/all HTTP/1.1
+//Page: 2
+//Sort: price
+//Order: desc
+//Expected curl response: GET Products: 2, price, desc
+app.get("/products/all", (req, res) => {
+  res.send(`GET Products: ${req.headers.page}, ${req.headers.sort}, ${req.headers.order}`);
+});
+
+//GET /products/8719 HTTP/1.1
+//Expected curl response: GET Products: 8719
+app.get("/products/:num", (req, res) => {
+  res.send(`GET Products: ${req.params.num}`);
+});
+
+//GET /products/8719 HTTP/1.1
+//Expected curl response: GET Products: 8719, small, red
+app.get("/products/:num-:size-:color", (req, res) => {
+  res.send(`GET Products: ${req.params.num}, ${req.params.size}, ${req.params.color}`);
+});
+
+app.listen(3000);
